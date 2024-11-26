a609bde54db652926cf8ad51ab44ffbe47736f76
johnnyFR26 <149261007+johnnyFR26@users.noreply.github.com>
2024-11-26 00:01:37 -0300

    edit server for vercel

--- server.ts
+++ server.ts
@@ -5,26 +5,27 @@ import { userRoutes } from "./routes/user.route";
 import { mainScreen } from "./lib/mainScreen";
 import cors from "@fastify/cors";
 
+// Cria o servidor Fastify
 const server = fastify();
 
+// Registra rotas
+server.register(taskRoutes);
+server.register(attachmentsRoutes);
+server.register(userRoutes);
 
-server.register(taskRoutes)
-server.register(attachmentsRoutes)
-server.register(userRoutes)
-
+// Configura CORS
 server.register(cors, {
-    origin: "*",
-})
-
-server.listen({ port: 3000 }, (err, address) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log(`Server listening on ${address}`);
+  origin: "*",
 });
 
+// Rota principal
 server.get("/", async (request, reply) => {
-    reply.type("text/html")
-     reply.send(mainScreen())
+  reply.type("text/html");
+  reply.send(mainScreen());
 });
+
+// Exporta o manipulador de requisições para a Vercel
+export default async (req: any, res: any) => {
+  await server.ready(); // Garante que o Fastify está pronto para processar requisições
+  server.server.emit("request", req, res);
+};
